663971b3068d7ff85afe1206fa100188df496752
Archit Purohit'git config --global user.name Archit <Architpurohit@hotmail.com>
2023-05-01 19:15:47 -0700

    included backpage

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -3,14 +3,18 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { BsSearch } from "react-icons/bs";
 import React from 'react';
+import Image from 'next/image';
+import 'tailwindcss/tailwind.css'
+
+
+
 
 export default function Home() {
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState({});
   const [loading, setLoading] = useState(false);
 const appid = process.env.NEXT_PUBLIC_WEATHER_KEY
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=vancouver&appid=${appid}`;
-
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=vancouver&appid=b074a274a35fca512da588073991e058`;
 
   const fetchWeather = (e) => {
     e.preventDefault();
@@ -30,7 +34,29 @@ const appid = process.env.NEXT_PUBLIC_WEATHER_KEY
         <title>Weather-Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <button onClick={fetchWeather}>fetch data</button>
+
+      <div className="relative">
+        <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/40 z-[1]' />
+        <Image
+          src="https://images.unsplash.com/photo-1592210454359-9043f067919b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          alt="A beautiful sunset over the mountains"
+          width={1080}
+          height={720}
+          className="object-cover"
+        />
+      </div>
+
+      <div className='relative flex justify-between items-center max-w-[500px] w-full m-auto pt-4 text-white z-10'>
+        <form className='flex justify-between items-center w-full m-auto p-3 bg-transparent border border-gray-300 text-white rounded-2xl'>
+          <div>
+            <input
+            onChange= {(e) => setCity(e.target.value)}
+            type="text" placeholder="search city"
+            />
+
+          </div>
+          <button onClick={fetchWeather}><BsSearch size={20}/></button>
+        </form>
+      </div>
     </div>
-  );
-}
+  )};

9923aa1faf154eb5fba59eb834f7c6015eb59da5
Archit Purohit'git config --global user.name Archit <Architpurohit@hotmail.com>
2023-05-02 10:55:23 -0700

    adding weather react file

---
+++ nextjs-blog/components/Weather.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Weather = ({data}) => {
+  console.log(data)
+  return (
+
+    <div>
+        <div>
+              <div>
+                {/* <Image alt='/'/> */}
+              </div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Weather
--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -5,7 +5,7 @@ import { BsSearch } from "react-icons/bs";
 import React from 'react';
 import Image from 'next/image';
 import 'tailwindcss/tailwind.css'
-
+import Weather from "../components/Weather";
 
 
 
@@ -14,7 +14,7 @@ export default function Home() {
   const [weather, setWeather] = useState({});
   const [loading, setLoading] = useState(false);
 const appid = process.env.NEXT_PUBLIC_WEATHER_KEY
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=vancouver&appid=b074a274a35fca512da588073991e058`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b074a274a35fca512da588073991e058`;
 
   const fetchWeather = (e) => {
     e.preventDefault();
@@ -22,7 +22,7 @@ const appid = process.env.NEXT_PUBLIC_WEATHER_KEY
     axios.get(url)
       .then((response) => {
         setWeather(response.data);
-        console.log(response.data);
+        // console.log(response.data);
       });
     setCity("");
     setLoading(false);
@@ -30,33 +30,36 @@ const appid = process.env.NEXT_PUBLIC_WEATHER_KEY
 
   return (
     <div>
-      <Head>
-        <title>Weather-Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Head>
+      <title>Weather-Next App</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
 
-      <div className="relative">
-        <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/40 z-[1]' />
-        <Image
-          src="https://images.unsplash.com/photo-1592210454359-9043f067919b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-          alt="A beautiful sunset over the mountains"
-          width={1080}
-          height={720}
-          className="object-cover"
+    <div className="bg-black/40 absolute top-0 left-0 right-0 bottom-0 z-[1]" />
+    <Image
+      src="https://images.unsplash.com/photo-1592210454359-9043f067919b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+      alt="A beautiful sunset over the mountains"
+      width={1080}
+      height={720}
+      className="object-cover"
+    />
+    <div className='relative flex justify-between items-center max-w-[500px] w-full m-auto pt-4 text-white z-10'>
+    <form onSubmit={fetchWeather}className='relative flex justify-between items-center max-w-[500px] w-full m-auto pt-4 text-white z-10'>
+      <div className='flex-1'>
+        <input
+          onChange={(e) => setCity(e.target.value)}
+          type="text"
+          placeholder="search city"
+          className='w-full p-3 bg-transparent border border-gray-300 text-white rounded-2xl'
         />
       </div>
-
-      <div className='relative flex justify-between items-center max-w-[500px] w-full m-auto pt-4 text-white z-10'>
-        <form className='flex justify-between items-center w-full m-auto p-3 bg-transparent border border-gray-300 text-white rounded-2xl'>
-          <div>
-            <input
-            onChange= {(e) => setCity(e.target.value)}
-            type="text" placeholder="search city"
-            />
-
-          </div>
-          <button onClick={fetchWeather}><BsSearch size={20}/></button>
-        </form>
-      </div>
+      <button onClick={fetchWeather}>
+        <BsSearch size={20} />
+      </button>
+    </form>
+  </div>
+ {/* weather */}
+    {weather.main && < Weather data={weather}/>}
     </div>
+
   )};
